b3520a29b6d3d83b5b2f2c13bf1e7dee0fdad7f2
bilgehanUnsal <bilgehanunsal@outlook.com.tr>
2023-09-21 23:41:20 +0300

    Bütün testler başarılı bir şekilde tamamlandı.

--- src/NopCommerceTest.java
+++ src/NopCommerceTest.java
@@ -1,12 +1,18 @@
 import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class NopCommerceTest extends BaseDriver {
 
-    @Test(groups = {"UserRegistiration", "Smoke"})
+    @Test(groups = {"UserRegistiration", "Smoke"}, priority = 1)
     public void Register() {
         Locators lc = new Locators();
 
@@ -17,26 +23,26 @@ public class NopCommerceTest extends BaseDriver {
         mySelectClass(lc.birthDay, "17");
         mySelectClass(lc.birthMonth, "August");
         mySelectClass(lc.birthYear, "1994");
-        mySendKeys(lc.emailInput, "bigteam14@gmail.com");
+        mySendKeys(lc.emailInput, "teteam14@gmail.com");
         mySendKeys(lc.passwordInput, "BigTeam14.");
         mySendKeys(lc.conPassInput, "BigTeam14.");
         myClick(lc.registerButton);
         verifyContainsText(lc.registerApplied, "Your registration completed");
     }
 
-    @Test(groups = {"LoginTest", "Smoke"})
+    @Test(groups = {"LoginTest", "Smoke"}, priority = 2)
     public void Login() {
         Locators lc = new Locators();
 
         myClick(lc.loginButton);
-        mySendKeys(lc.logInMail, "bigteam14@gmail.com");
+        mySendKeys(lc.logInMail, "teteam14@gmail.com");
         mySendKeys(lc.logInPassword, "BigTeam14.");
         myClick(lc.getLogIn);
 
         verifyContainsText(lc.myAccount, "my account");
     }
 
-    @Test(groups = {"UI Testing", "TAB Menu", "Sipariş Testleri"})
+    @Test(groups = {"UI Testing", "TAB Menu", "Sipariş Testleri"}, priority = 3)
     public void GiftTest() {
         Locators lc = new Locators();
 
@@ -50,7 +56,7 @@ public class NopCommerceTest extends BaseDriver {
         verifyContainsText(lc.giftVerify, "shopping cart");
     }
 
-    @Test(groups = {"UI Testing", "TAB Menu", "Sipariş Testleri"})
+    @Test(groups = {"UI Testing", "TAB Menu", "Sipariş Testleri"}, priority = 4)
     public void BuyingComputer() {
         Locators lc = new Locators();
 
@@ -67,7 +73,7 @@ public class NopCommerceTest extends BaseDriver {
         verifyContainsText(lc.verifyComputer, "shopping cart");
     }
 
-    @Test(groups = {"UI Testing", "TAB Menu"})
+    @Test(groups = {"UI Testing", "TAB Menu"}, priority = 5)
     public void TabMenu() {
         Locators lc = new Locators();
 
@@ -119,7 +125,7 @@ public class NopCommerceTest extends BaseDriver {
         lc.giftcard.click();
         wait.until(ExpectedConditions.visibilityOf(lc.dogrulama));
     }
-    @Test (groups = {"UI Testing", "Search", "Smoke", "Regression"})
+    @Test (groups = {"UI Testing", "Search", "Smoke Test", "Regression"}, priority = 6)
     @Parameters("searchText")
     public void ParametersSearch(String arananKelime) {
         Locators lc = new Locators();
@@ -127,9 +133,10 @@ public class NopCommerceTest extends BaseDriver {
         mySendKeys(lc.searchStore, arananKelime);
         myClick(lc.searchButton);
 
+        scrollToElement(lc.adobeVerify);
         verifyContainsText(lc.adobeVerify, "Adobe Photoshop CS4");
     }
-    @Test(groups = {"Smoke Test"}, dataProvider = "UserData")
+    @Test(groups = {"Smoke Test", "LoginTest"}, dataProvider = "UserData")
     public void hataliGirisTest(String mail, String password) {
 
         Locators lc = new Locators();
@@ -137,9 +144,14 @@ public class NopCommerceTest extends BaseDriver {
         myClick(lc.loginButton);
         mySendKeys(lc.logInMail, mail);
         mySendKeys(lc.logInPassword, password);
+        myClick(lc.getLogIn);
 
-        verifyContainsText(lc.unSucLogIn, "No customer account found");
-
+        List<WebElement> logIn = driver.findElements(By.cssSelector("[class='message-error validation-summary-errors'] ul li"));
+        if (logIn.size()>0){
+            verifyContainsText(lc.unSucLogIn, "No customer account found");
+        } else {
+            verifyContainsText(lc.myAccount, "my account");
+        }
     }
 
     @DataProvider
@@ -151,7 +163,7 @@ public class NopCommerceTest extends BaseDriver {
                 {"deniz@gmail.com", "11111"},
                 {"bilal@gmail.com", "11111"},
                 {"hakan@gmail.com", "11111"},
-                {"bigteam14@gmail.com", "BigTeam14."}
+                {"teteam14@gmail.com", "BigTeam14."}
         };
         return data;
 
